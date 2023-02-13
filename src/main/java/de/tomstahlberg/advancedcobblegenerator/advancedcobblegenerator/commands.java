fe8d2af356f667305e81ab943829c464600df50e
Tom Stahlberg <tom.stahlberg@jakkit.de>
2023-02-14 00:37:50 +0100

    update

--- src/main/java/de/tomstahlberg/advancedcobblegenerator/advancedcobblegenerator/commands.java
+++ src/main/java/de/tomstahlberg/advancedcobblegenerator/advancedcobblegenerator/commands.java
@@ -1,10 +1,14 @@
 package de.tomstahlberg.advancedcobblegenerator.advancedcobblegenerator;
 
+import de.tomstahlberg.advancedcobblegenerator.advancedcobblegenerator.functions.Configuration;
+import de.tomstahlberg.advancedcobblegenerator.advancedcobblegenerator.functions.GeneratorMap;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 public class commands implements CommandExecutor {
     @Override
@@ -13,6 +17,18 @@ public class commands implements CommandExecutor {
             if(args.length == 1){
                 if(args[0].equalsIgnoreCase("reload")){
                     //reload
+                    try {
+                        Main.configurator = new Configuration();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    GeneratorMap genMap = new GeneratorMap(Main.configurator.getGeneratorConfiguration());
+                    Main.generatorMap = genMap.getGeneratorMap();
+                    Main.plugin.getServer().getConsoleSender().sendMessage(Main.generatorMap.toString());
+
+                    Main.settings = Main.configurator.getSettingsConfiguration();
+
+                    Main.defaultBiome = genMap.getDefaultBiome();
                 }else{
                     sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6&lGolden&3&lSky &8x &cBenutze /acg reload."));
                 }
